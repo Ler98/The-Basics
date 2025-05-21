0e07538ee23a174a88516e7c33f124fef34e39db
Ler98 <lisa.eriksson98@hotmail.com>
2025-05-21 13:49:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 let frukter = ["apelsin", "äpple", "banan", "kiwi", "citron"];
 
 //Gör en array som innehåller 3 olika datatyper: string, number, array.
-let blandat = ["apelsin", 10, frukter]
+let blandat = ["apelsin", 10, frukter];
 
 //Hur många objekt innehåller arrayen under?
-let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua']
+let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua'];
     //svaret blev 4 i console
     
 
@@ -67,4 +67,43 @@ fruits.pop();
 fruits.shift();
     console.log(fruits);
 
+//sätt in en frukt på index 1.
+fruits.splice(1, 0, "jordgubbe");
+    console.log(fruits);
 
+//lägg till tre frukter på index 2.
+fruits.splice(2, 0, "päron, päron, päron");
+    console.log(fruits);
+
+
+let names = ['David', 'Christoffer', 'Johan', 'Maja']
+//la bort christoffer och johan.
+names.splice(1, 2,);
+    console.log(names);
+
+//spegelvänd arrayen.
+let nums = [1,2,3,4,5,6,7,8,9];
+nums.reverse();
+    console.log(nums);
+
+//kolla om n finns i strängen.
+let str = 'Supercalifragilisticexpialidocious';
+let finnsN = str.includes("n");
+    console.log(finnsN);
+
+//kolla om x finns i strängen.
+let finnsX = str.includes("x");
+    console.log(finnsX);
+
+//I ovanstående array, hitta vilket position första förekomsten av p har.
+let finnsP = names.some(namn => namn.includes("p"))
+    // p fanns inte, så blev falskt
+    console.log(finnsP);
+
+//skriv ut de 5 första bokstäverna i strängen ovan.
+let femForsta = str.slice(0, 5);
+    console.log(femForsta);
+
+//skriv ut de 7 första bokstäverna i strängen ovan.
+let sjuForsta = str.slice(0, 7);
+    console.log(sjuForsta);
